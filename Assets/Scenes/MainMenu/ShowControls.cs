edd7191bafe4f11c2df0cafc67a82b6e434ef29f
Nimus1 <nimus100@proton.me>
2023-02-05 15:28:03 +0100

    Added controls lookup

---
+++ Assets/Scenes/MainMenu/ShowControls.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShowControls : MonoBehaviour
+{
+    [SerializeField] GameObject controlsPanel;
+    [SerializeField] KeyCode showControlsKey = KeyCode.T;
+    [SerializeField] private string mainMenuScene;
+    private bool areControlsVisible = false;
+
+    private void Start()
+    {
+        controlsPanel.SetActive(areControlsVisible);
+    }
+
+    private void Update()
+    {
+        if(UnityEngine.Input.GetKeyDown(showControlsKey))
+        {
+            controlsPanel.SetActive(areControlsVisible);
+            areControlsVisible =! areControlsVisible;
+        }
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+}
